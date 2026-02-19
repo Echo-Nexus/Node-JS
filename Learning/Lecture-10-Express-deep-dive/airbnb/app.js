1f6838a6e1cfc7787574e2088c4f2df27518b73a
Echo-Nexus <vxattitude@gmail.com>
2026-02-19 12:22:08 +0545

    Added lecture 10

---
+++ Learning/Lecture-10-Express-deep-dive/airbnb/app.js
@@ -0,0 +1,29 @@
+// Core Module
+const path = require('path');
+// External Module
+const express = require("express");
+
+// Local Modules
+const userRouter = require("./routers/user-router");
+const hostRouter = require("./routers/host-router");
+const rootDir = require("./utils/pathUtil");
+
+ 
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.url, req.method);
+  next();
+});
+
+app.use(express.urlencoded());
+app.use(userRouter);
+app.use("/host", hostRouter);
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+})
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
