47651a0713c7b34126d9f8e6ec79778cf86d9889
Echo-Nexus <vxattitude@gmail.com>
2026-02-20 10:57:38 +0545

    Added Chapter 11

---
+++ Learning/Lecture-11-Tailwind-CSS-styling-airbnb/airbnb/routers/user-router.js
@@ -0,0 +1,12 @@
+// Core module
+const path = require('path');
+// External module
+const express = require('express');
+const userRouter = express.Router();
+const rootDir = require('../utils/pathUtil');
+
+userRouter.get("/", (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'home.html'));
+});
+
+module.exports = userRouter;
